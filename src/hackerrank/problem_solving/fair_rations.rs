6ec4da6f07cd3aec2e0c3c654eef35b1317cc5b3
clD11 <23483715+cld11@users.noreply.github.com>
2020-11-25 22:05:54 +0000

    WIP - fair rations

---
+++ src/hackerrank/problem_solving/fair_rations.rs
@@ -0,0 +1,25 @@
+pub fn fair_rations(arr: &[u32]) -> &'static str {
+    if arr[0] % 2 != 0 && arr[1] % 2 == 0 {
+        return "NO";
+    }
+
+    // count
+    // for i = arr[arr.length - 2] >= 1; --i
+    // if arr[i + 1] % 2 != 0 increment until arr[i + 1]++ && arr[i]++; count++;
+
+    "4"
+}
+
+#[test]
+fn shouldReturnNo() {
+    let arr: [u32; 2] = [1, 2];
+    let actual = fair_rations(&arr);
+    assert_eq!(actual, "NO");
+}
+
+#[test]
+fn shouldReturnNumber() {
+    let arr: [u32; 5] = [2, 3, 4, 5, 6];
+    let actual = fair_rations(&arr);
+    assert_eq!(actual, "4");
+}
--- src/hackerrank/problem_solving/mod.rs
+++ src/hackerrank/problem_solving/mod.rs
@@ -3,3 +3,4 @@ pub mod divisible_sum_pairs;
 pub mod distinct_pairs;
 pub mod number_converter;
 pub mod find_digits;
+pub mod fair_rations;
