3a2dac19fa43e5953a6cb7a95921811d462d8f77
clD11 <23483715+cld11@users.noreply.github.com>
2021-01-20 22:59:41 +0000

    shortest reach

---
+++ src/hackerrank/problem_solving/breadth_first_search_shortest_reach.rs
@@ -0,0 +1,65 @@
+use std::collections::LinkedList;
+use std::cmp::Ordering::Greater;
+
+#[derive(Debug)]
+struct Graph {
+    adj: Vec<Vec<usize>>,
+}
+
+impl Graph {
+
+    fn build(v :usize) -> Graph {
+        let mut graph = Graph {
+            adj: vec![vec![]; v]
+        };
+        graph
+    }
+
+    fn add_edge(&mut self, v : usize, w: usize) {
+        self.adj[v].push(w);
+        self.adj[w].push(v);
+    }
+
+    fn adj(&mut self, v: usize) -> &Vec<usize> {
+        return &self.adj[v];
+    }
+
+}
+
+fn find_shortest_reach() -> Vec<usize> {
+
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn should_initialize_graph() {
+        let vertices = 5;
+        let actual = Graph::build(vertices);
+        assert_eq!(actual.adj.len(), vertices);
+    }
+
+    #[test]
+    fn should_add_edge() {
+        let vertices = 5;
+        let mut actual = Graph::build(vertices);
+        actual.add_edge(0, 1);
+        assert_eq!(actual.adj[0][0], 1);
+        assert_eq!(actual.adj[1][0], 0);
+    }
+
+    #[test]
+    fn should_get_adj_for_vertex() {
+        let vertices = 5;
+        let mut graph = Graph::build(vertices);
+        let vertex = 0;
+        graph.add_edge(vertex, 1);
+        let actual = graph.adj(vertex);
+        assert_eq!(actual.len(), 1);
+        assert_eq!(actual[0], 1);
+    }
+
+}
--- src/hackerrank/problem_solving/mod.rs
+++ src/hackerrank/problem_solving/mod.rs
@@ -9,3 +9,4 @@ pub mod fizz_buzz;
 pub mod ice_cream_parlor;
 pub mod missing_numbers;
 pub mod sherlock_and_array;
+pub mod breadth_first_search_shortest_reach;
