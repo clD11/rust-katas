822cfa20938889b460bdb95410cca09ff7dc7c7e
clD11 <23483715+clD11@users.noreply.github.com>
2021-05-20 22:35:19 +0100

    append and delete

--- src/hackerrank/problem_solving/append_and_delete.rs
+++ src/hackerrank/problem_solving/append_and_delete.rs
@@ -1,7 +1,16 @@
 fn append_and_delete(initial: &str, desired: &str, operation: u32) -> &'static str {
-    // let start_index: u32 = 0;
-    // while n
-    "yes"
+    let mut start_index: usize = 0;
+    while start_index < initial.len() &&
+        start_index < desired.len() &&
+        initial.as_bytes()[start_index] == desired.as_bytes()[start_index] {
+        start_index += 1;
+    }
+
+    if initial.len() - start_index + desired.len() - start_index <= operation as usize {
+        return "yes"
+    }
+
+    "no"
 }
 
 #[cfg(test)]
@@ -17,11 +26,19 @@ mod test {
         assert_eq!(append_and_delete(&initial, &desired, operation), "yes");
     }
 
-    // #[test]
-    // fn should_return_false_shorter() {
-    //     let initial = String::from("ashley");
-    //     let desired = String::from("ash");
-    //     let operation: u32 = 2;
-    //     assert_eq!(append_and_delete(&initial, &desired, operation), "no");
-    // }
+    #[test]
+    fn should_return_false_shorter() {
+        let initial = String::from("ashley");
+        let desired = String::from("ash");
+        let operation: u32 = 2;
+        assert_eq!(append_and_delete(&initial, &desired, operation), "no");
+    }
+
+    #[test]
+    fn should_return_true_equal() {
+        let initial = String::from("aba");
+        let desired = String::from("aba");
+        let operation: u32 = 7;
+        assert_eq!(append_and_delete(&initial, &desired, operation), "yes");
+    }
 }
