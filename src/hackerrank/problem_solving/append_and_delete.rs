c96384697af2ee38afbd50a9a4f89b0e1f68589d
clD11 <23483715+clD11@users.noreply.github.com>
2021-05-21 18:14:48 +0100

    append and delete

--- src/hackerrank/problem_solving/append_and_delete.rs
+++ src/hackerrank/problem_solving/append_and_delete.rs
@@ -1,15 +1,15 @@
-fn append_and_delete(initial: &str, desired: &str, operation: u32) -> &'static str {
-    let mut start_index: usize = 0;
-    while start_index < initial.len() &&
-        start_index < desired.len() &&
-        initial.as_bytes()[start_index] == desired.as_bytes()[start_index] {
-        start_index += 1;
+fn append_and_delete(initial: &str, desired: &str, operation: usize) -> &'static str {
+    let shortest: usize = if initial.len() < desired.len() { initial.len() } else { desired.len() };
+    let mut index = 0;
+    for i  in 0..shortest {
+        if initial.as_bytes()[i] != desired.as_bytes()[i] {
+            index = i;
+            break;
+        }
     }
-
-    if initial.len() - start_index + desired.len() - start_index <= operation as usize {
+    if  initial.len() + desired.len() - index * 2 <= operation {
         return "yes"
     }
-
     "no"
 }
 
@@ -22,7 +22,7 @@ mod test {
     fn should_return_true_shorter() {
         let initial = String::from("hackerhappy");
         let desired = String::from("hackerrank");
-        let operation: u32 = 9;
+        let operation: usize = 9;
         assert_eq!(append_and_delete(&initial, &desired, operation), "yes");
     }
 
@@ -30,7 +30,7 @@ mod test {
     fn should_return_false_shorter() {
         let initial = String::from("ashley");
         let desired = String::from("ash");
-        let operation: u32 = 2;
+        let operation: usize = 2;
         assert_eq!(append_and_delete(&initial, &desired, operation), "no");
     }
 
@@ -38,7 +38,7 @@ mod test {
     fn should_return_true_equal() {
         let initial = String::from("aba");
         let desired = String::from("aba");
-        let operation: u32 = 7;
+        let operation: usize = 7;
         assert_eq!(append_and_delete(&initial, &desired, operation), "yes");
     }
 }
