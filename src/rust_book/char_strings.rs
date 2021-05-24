04519b18c9894801fc9693e6e3e04b3c7bf1b78a
clD11 <23483715+clD11@users.noreply.github.com>
2021-05-24 09:12:21 +0100

    str and string

--- src/rust_book/char_strings.rs
+++ src/rust_book/char_strings.rs
@@ -1,5 +1,17 @@
 fn mask_character(target: &str, input: &str) -> String {
-    String::from("aaaaaa")
+    let mut characters: Vec<String> = input
+        .split("")
+        .filter(|c| *c != "")
+        .map(|c| c.to_owned())
+        .collect();
+
+    for i in 0..characters.len() {
+        if characters[i] == target {
+            characters[i] = String::from("#");
+        }
+    }
+
+    characters.join("")
 }
 
 #[cfg(test)]
@@ -12,7 +24,7 @@ mod tests {
         let target = String::from("z");
         let input = String::from("aaazaaa");
         let actual = mask_character(&target, &input);
-        assert_eq!(actual, "aaaaaa");
+        assert_eq!(actual, "aaa#aaa");
     }
 
 }
