3bea90de5d0ae718c39418469d908a09c934ab8f
clD11 <23483715+clD11@users.noreply.github.com>
2022-07-06 22:26:33 +0100

    async

--- src/multithreading/async_book.rs
+++ src/multithreading/async_book.rs
@@ -1,13 +1,21 @@
 use async_std::task;
 use futures::executor::block_on;
 
-use std::{
-    future::Future,
-    pin::Pin,
-    sync::{Arc, Mutex},
-    task::{Context, Poll, Waker},
-    thread,
-    time::Duration,
+use {
+    futures::{
+        future::{BoxFuture, FutureExt},
+        task::{waker_ref, ArcWake},
+    },
+    std::{
+        future::Future,
+        pin::Pin,
+        sync::atomic::compiler_fence,
+        sync::mpsc::{sync_channel, Receiver, SyncSender},
+        sync::{Arc, Mutex},
+        task::{Context, Poll, Waker},
+        thread,
+        time::Duration,
+    },
 };
 
 // async builds state machine that implements Future
@@ -50,7 +58,59 @@ pub struct TimerFuture {
     shared_state: Arc<Mutex<SharedState>>,
 }
 
+struct SharedState {
+    completed: bool,
+    waker: Option<Waker>,
+}
 
+impl TimerFuture {
+    pub fn new(duration: Duration) -> TimerFuture {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None,
+        }));
+
+        let thread_shared_state = shared_state.clone();
+        thread::spawn(move || {
+            thread::sleep(duration);
+            let mut shared_state = thread_shared_state.lock().unwrap();
+            shared_state.completed = true;
+            if let Some(waker) = shared_state.waker.take() {
+                waker.wake();
+            }
+        });
+
+        TimerFuture { shared_state }
+    }
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+// Executor
+
+struct Executor {
+    ready_queue: Receiver<Arc<Task>>,
+}
+
+#[derive(Clone)]
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+struct Task {
+
+}
 
 #[cfg(test)]
 mod tests {
